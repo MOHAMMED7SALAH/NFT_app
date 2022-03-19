61e180d33ecdc6c4614c2c3a257f3a31754a2e46
MOHAMMED7SALAH <bentitimohammedsalahstars7@gmail.com>
2022-03-19 10:16:32 +0100

    first_NFT

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:ntf_app/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NFTs',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: const HomeScreen(),
+    );
+  }
+}
